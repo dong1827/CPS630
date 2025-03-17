df2973dcbe10d3ce1938c0a58817770bb6f36d9b
AradD7 <arad.delaramnikoo@torontomu.ca>
2025-03-17 19:49:38 -0400

    fixed app.js

--- lab07/app.js
+++ lab07/app.js
@@ -1,21 +1,41 @@
 var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider) {
+
+app.config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.hashPrefix('');
     $routeProvider
+
     .when('/', {
     templateUrl : 'pages/home.html',
-    controller : 'HomeController'})
+    controller : 'HomeController'
+    })
+
     .when('/aboutus', {
     templateUrl : 'pages/aboutus.html',
-    controller : 'AboutusController'})
+    controller : 'AboutusController'
+    })
+
     .when('/reviews', {
     templateUrl : 'pages/reviews.html',
-    controller : 'ReviewsController'})
-    .otherwise({redirectTo: '/'});
+    controller : 'ReviewsController'
+    })
+
+    .otherwise({
+        redirectTo: '/'
+    });
+
 });
 
 app.controller('HomeController', function($scope) {
-    $scope.message = 'Hello from HomeController';});
-    app.controller('AboutusController', function($scope) {
-    $scope.message = 'Hello from AboutusController';});
-    app.controller('ReviewsController', function($scope) {
-    $scope.message = 'Hello from ReviewsController';});
+    $scope.message = 'Hello from HomeController';
+});
+
+app.controller('AboutusController', function($scope) {
+    $scope.message = 'Hello from AboutusController';
+});
+
+app.controller('ReviewsController', function($scope) {
+    $scope.message = 'Hello from ReviewsController';}
+);
+
+
